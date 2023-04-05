9163dbd6e2473f3d081dee8c956212391b5157e3
duguorong <dudurong009@gmail.com>
2023-04-05 15:57:17 +0800

    Add the "Bits" and "AssignedBits" impl from "sha256" code

--- ripemd160-circuit/src/table16.rs
+++ ripemd160-circuit/src/table16.rs
@@ -2,7 +2,11 @@
 Based on code from https://github.com/privacy-scaling-explorations/halo2/blob/8c945507ceca5f4ed6e52da3672ea0308bcac812/halo2_gadgets/src/sha256/table16.rs
 */
 
-use halo2_proofs::circuit::Value;
+use halo2_proofs::{
+    circuit::{AssignedCell, Region, Value},
+    halo2curves::pasta::pallas,
+    plonk::{Any, Assigned, Column, Error},
+};
 
 pub(crate) mod util;
 
@@ -15,5 +19,181 @@ pub struct BlockWord(pub Value<u32>);
 impl From<u32> for BlockWord {
     fn from(x: u32) -> Self {
         BlockWord(Value::known(x))
+    }
+}
+
+/// Little-endian bits (up to 64 bits)
+#[derive(Debug, Clone)]
+pub struct Bits<const LEN: usize>([bool; LEN]);
+
+impl<const LEN: usize> Bits<LEN> {
+    fn spread<const SPREAD: usize>(&self) -> [bool; SPREAD] {
+        spread_bits(self.0)
+    }
+}
+
+impl<const LEN: usize> std::ops::Deref for Bits<LEN> {
+    type Target = [bool; LEN];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<const LEN: usize> From<[bool; LEN]> for Bits<LEN> {
+    fn from(bits: [bool; LEN]) -> Self {
+        Self(bits)
+    }
+}
+
+impl<const LEN: usize> From<&Bits<LEN>> for [bool; LEN] {
+    fn from(bits: &Bits<LEN>) -> Self {
+        bits.0
+    }
+}
+
+impl<const LEN: usize> From<&Bits<LEN>> for Assigned<pallas::Base> {
+    fn from(bits: &Bits<LEN>) -> Self {
+        assert!(LEN <= 64);
+        pallas::Base::from(lebs2ip(&bits.0)).into()
+    }
+}
+
+impl From<&Bits<16>> for u16 {
+    fn from(bits: &Bits<16>) -> Self {
+        lebs2ip(&bits.0) as u16
+    }
+}
+
+impl From<u16> for Bits<16> {
+    fn from(value: u16) -> Self {
+        Bits(i2lebsp::<16>(value.into()))
+    }
+}
+
+impl From<&Bits<32>> for u32 {
+    fn from(bits: &Bits<32>) -> Self {
+        lebs2ip(&bits.0) as u32
+    }
+}
+
+impl From<u32> for Bits<32> {
+    fn from(value: u32) -> Self {
+        Bits(i2lebsp::<32>(value.into()))
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct AssignedBits<const LEN: usize>(AssignedCell<Bits<LEN>, pallas::Base>);
+
+impl<const LEN: usize> std::ops::Deref for AssignedBits<LEN> {
+    type Target = AssignedCell<Bits<LEN>, pallas::Base>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<const LEN: usize> AssignedBits<LEN> {
+    fn assign_bits<A, AR, T: TryInto<[bool; LEN]> + std::fmt::Debug + Clone>(
+        region: &mut Region<'_, pallas::Base>,
+        annotation: A,
+        column: impl Into<Column<Any>>,
+        offset: usize,
+        value: Value<T>,
+    ) -> Result<Self, Error>
+    where
+        A: Fn() -> AR,
+        AR: Into<String>,
+        <T as TryInto<[bool; LEN]>>::Error: std::fmt::Debug,
+    {
+        let value: Value<[bool; LEN]> = value.map(|v| v.try_into().unwrap());
+        let value: Value<Bits<LEN>> = value.map(|v| v.into());
+
+        let column: Column<Any> = column.into();
+        match column.column_type() {
+            Any::Advice(_) => {
+                region.assign_advice(annotation, column.try_into().unwrap(), offset, || {
+                    value.clone()
+                })
+            }
+            Any::Fixed => {
+                region.assign_fixed(annotation, column.try_into().unwrap(), offset, || {
+                    value.clone()
+                })
+            }
+            _ => panic!("Cannot assign to instance column"),
+        }
+        .map(AssignedBits)
+    }
+}
+
+impl AssignedBits<16> {
+    fn value_u16(&self) -> Value<u16> {
+        self.value().map(|v| v.into())
+    }
+
+    fn assign<A, AR>(
+        region: &mut Region<'_, pallas::Base>,
+        annotation: A,
+        column: impl Into<Column<Any>>,
+        offset: usize,
+        value: Value<u16>,
+    ) -> Result<Self, Error>
+    where
+        A: Fn() -> AR,
+        AR: Into<String>,
+    {
+        let column: Column<Any> = column.into();
+        let value: Value<Bits<16>> = value.map(|v| v.into());
+        match column.column_type() {
+            Any::Advice(_) => {
+                region.assign_advice(annotation, column.try_into().unwrap(), offset, || {
+                    value.clone()
+                })
+            }
+            Any::Fixed => {
+                region.assign_fixed(annotation, column.try_into().unwrap(), offset, || {
+                    value.clone()
+                })
+            }
+            _ => panic!("Cannot assign to instance column"),
+        }
+        .map(AssignedBits)
+    }
+}
+
+impl AssignedBits<32> {
+    fn value_u32(&self) -> Value<u32> {
+        self.value().map(|v| v.into())
+    }
+
+    fn assign<A, AR>(
+        region: &mut Region<'_, pallas::Base>,
+        annotation: A,
+        column: impl Into<Column<Any>>,
+        offset: usize,
+        value: Value<u32>,
+    ) -> Result<Self, Error>
+    where
+        A: Fn() -> AR,
+        AR: Into<String>,
+    {
+        let column: Column<Any> = column.into();
+        let value: Value<Bits<32>> = value.map(|v| v.into());
+        match column.column_type() {
+            Any::Advice(_) => {
+                region.assign_advice(annotation, column.try_into().unwrap(), offset, || {
+                    value.clone()
+                })
+            }
+            Any::Fixed => {
+                region.assign_fixed(annotation, column.try_into().unwrap(), offset, || {
+                    value.clone()
+                })
+            }
+            _ => panic!("Cannot assign to instance column"),
+        }
+        .map(AssignedBits)
     }
 }
